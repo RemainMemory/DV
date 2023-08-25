80c161dd18c42d64484c078007aa9677a1e4cff5
Remain <chenfeiyu592@gmail.com>
2023-08-25 15:04:58 +0800

    adjust spider.py

--- Data_Show/splider/spider.py
+++ Data_Show/splider/spider.py
@@ -5,207 +5,105 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-# 爬取国家统计局数据
-# 居民人均可支配收入(元)
-def spider_disposable_income_of_residents():
-    url = "https://data.stats.gov.cn/search.htm"
-    rate_list = []  # 使用一个新列表来保存所有数据
-    for year in (range(2014, 2022)):
-        parameters = {
-            "s": f"{year}可支配收入",  # f{year}代表格式化字符串
-            "m": "searchdata",
-            "db": "",
-            "p": "0"
-        }
-        time.sleep(1)
-        try:
-            # 模拟发送请求
-            response = requests.get(url, params=parameters, verify=False)
-            response.raise_for_status()
-            # 获取网页状态码
-            data = response.json()
-            rate_list.append((year, data["result"][0]["data"]))
-            print(f"{year}年居民可支配收入：{data['result'][0]['data']}")
+def fetch_data(url, search_query, year, quarters=None):
+    data_dict = {}
+    parameters = {
+        "s": f"{year}{search_query}",
+        "m": "searchdata",
+        "db": "",
+        "p": "0"
+    }
+    time.sleep(1)
+    try:
+        response = requests.get(url, params=parameters, verify=False)
+        response.raise_for_status()
+        data = response.json()
 
-        except (requests.RequestException, KeyError):
-            print(f"Failed to fetch data for {year}")
+        if quarters and any(province in search_query for province in ["云南省", "贵州省", "四川省",
+                                                                      "广西壮族自治区", "重庆市", "西藏自治区",
+                                                                      "陕西省", "甘肃省", "青海省", "宁夏回族自治区",
+                                                                      "新疆维吾尔自治区", "内蒙古自治区", "黑龙江省",
+                                                                      "吉林省", "辽宁省", "河北省", "山西省", "山东省",
+                                                                      "河南省", "江苏省", "安徽省", "浙江省", "福建省",
+                                                                      "江西省", "湖南省", "湖北省", "广东省", "海南省",
+                                                                      "北京市", "天津市", "上海市"]):
+            data_dict[search_query] = data["result"][3]["data"]  # 只获取第四行数据
+        elif quarters:
+            quarter_data_dict = {}
+            for i, quarter_data in enumerate(data["result"][:4][::-1]):
+                quarter_data_dict[quarters[i]] = quarter_data["data"]
+            data_dict[search_query] = quarter_data_dict
+        else:
+            data_dict[search_query] = data["result"][0]["data"]
 
+        print(f"Fetched data for {year} - {search_query}: {data_dict[search_query]}")
 
-# 居民人均可支配收入累计值(元)(季度)
-def spider_disposable_income_of_residents_cumulative_value():
-    url = "https://data.stats.gov.cn/search.htm"
-    rate_list = []  # 使用一个新列表来保存所有数据
-    quarters = ["一", "二", "三", "四"]
-    for year in range(2020, 2022):
-        parameters = {
-            "s": f"{year}年居民可支配收入累计值",  # 搜索整年的数据
-            "m": "searchdata",
-            "db": "",
-            "p": "0"
-        }
-        time.sleep(1)  # 休眠1秒以防止被封
-        try:
-            # 模拟发送请求
-            response = requests.get(url, params=parameters, verify=False)
-            response.raise_for_status()
-            # 获取网页状态码
-            data = response.json()
-
-            # 假设data["result"]是一个包含四个季度数据的列表
-            for i, quarter_data in enumerate(data["result"][:4][::-1]):  # 逆序处理
-                rate_list.append((year, quarters[i], quarter_data["data"]))
-                print(f"{year}年第{quarters[i]}季度居民可支配收入累计值：{quarter_data['data']}")
-
-        except (requests.RequestException, KeyError):
-            print(f"Failed to fetch data for {year}")
+    except (requests.RequestException, KeyError):
+        print(f"Failed to fetch data for {year}")
+    return data_dict
 
 
-# 居民人均可支配收入累计增长(季度)
-def spider_disposable_income_of_residents_cumulative_growth():
-    url = "https://data.stats.gov.cn/search.htm"
-    rate_list = []  # 使用一个新列表来保存所有数据
-    quarters = ["一", "二", "三", "四"]
-    for year in range(2020, 2022):
-        parameters = {
-            "s": f"{year}年居民可支配收入累计增长",  # 搜索整年的数据
-            "m": "searchdata",
-            "db": "",
-            "p": "0"
-        }
-        time.sleep(1)  # 休眠1秒以防止被封
-        try:
-            # 模拟发送请求
-            response = requests.get(url, params=parameters, verify=False)
-            response.raise_for_status()
-            # 获取网页状态码
-            data = response.json()
+url = "https://data.stats.gov.cn/search.htm"
+quarters = ["一", "二", "三", "四"]
 
-            # 假设data["result"]是一个包含四个季度数据的列表
-            for i, quarter_data in enumerate(data["result"][:4][::-1]):  # 逆序处理
-                rate_list.append((year, quarters[i], quarter_data["data"]))
-                print(f"{year}年第{quarters[i]}季度居民人均可支配收入_累计增长(%)：{quarter_data['data']}")
+queries = [
+    "全国居民人均可支配收入",
+    "云南省 居民人均可支配收入",
+    "贵州省 居民人均可支配收入",
+    "四川省 居民人均可支配收入",
+    "广西壮族自治区 居民人均可支配收入",
+    "重庆市 居民人均可支配收入",
+    "西藏自治区 居民人均可支配收入",
+    "陕西省 居民人均可支配收入",
+    "甘肃省 居民人均可支配收入",
+    "青海省 居民人均可支配收入",
+    "宁夏回族自治区 居民人均可支配收入",
+    "新疆维吾尔自治区 居民人均可支配收入",
+    "内蒙古自治区 居民人均可支配收入",
+    "黑龙江省 居民人均可支配收入",
+    "吉林省 居民人均可支配收入",
+    "辽宁省 居民人均可支配收入",
+    "河北省 居民人均可支配收入",
+    "山西省 居民人均可支配收入",
+    "山东省 居民人均可支配收入",
+    "河南省 居民人均可支配收入",
+    "江苏省 居民人均可支配收入",
+    "安徽省 居民人均可支配收入",
+    "浙江省 居民人均可支配收入",
+    "福建省 居民人均可支配收入",
+    "江西省 居民人均可支配收入",
+    "湖南省 居民人均可支配收入",
+    "湖北省 居民人均可支配收入",
+    "广东省 居民人均可支配收入",
+    "海南省 居民人均可支配收入",
+    "北京市 居民人均可支配收入",
+    "天津市 居民人均可支配收入",
+    "上海市 居民人均可支配收入",
+    "居民人均可支配收入基尼系数",
+    "居民人均可支配收入_累计值",
+    "居民人均可支配收入_累计增长",
+    "居民可支配收入中位数",
+    "居民可支配收入中位数_累计值",
+    "居民可支配收入中位数_累计增长",
+    "居民人均可支配工资性收入_累计值",
+    "居民人均可支配工资性收入_累计增长",
+    "居民人均可支配经营净收入_累计值",
+    "居民人均可支配经营净收入_累计增长",
+    "居民人均可支配财产净收入_累计值",
+    "居民人均可支配财产净收入_累计增长",
+    "居民人均可支配转移净收入_累计值",
+    "居民人均可支配转移净收入_累计增长"
+]
 
-        except (requests.RequestException, KeyError):
-            print(f"Failed to fetch data for {year}")
+all_data_by_year = {}
+for year in range(2014, 2022):
+    year_data = {}
+    for query in queries:
+        if "年" in query or "累计" in query:
+            year_data.update(fetch_data(url, query, year, quarters))
+        else:
+            year_data.update(fetch_data(url, query, year))
+    all_data_by_year[year] = year_data
 
-
-# 居民人均可支配收入中位数(元)
-def spider_Median_per_capita_disposable_income_of_residents():
-    url = "https://data.stats.gov.cn/search.htm"
-    rate_list = []  # 使用一个新列表来保存所有数据
-    for year in (range(2020, 2022)):
-        parameters = {
-            "s": f"{year}居民可支配收入中位数",  # f{year}代表格式化字符串
-            "m": "searchdata",
-            "db": "",
-            "p": "0"
-        }
-        time.sleep(1)
-        try:
-            # 模拟发送请求
-            response = requests.get(url, params=parameters, verify=False)
-            response.raise_for_status()
-            # 获取网页状态码
-            data = response.json()
-            rate_list.append((year, data["result"][0]["data"]))
-            print(f"{year}年居民可支配收入中位数：{data['result'][0]['data']}")
-        except (requests.RequestException, KeyError):
-            print(f"Failed to fetch data for {year}")
-
-
-# 居民人均可支配收入中位数累计值(元)(季度)
-def spider_Median_per_capita_disposable_income_of_residents_cumulative_value():
-    url = "https://data.stats.gov.cn/search.htm"
-    rate_list = []  # 使用一个新列表来保存所有数据
-    quarters = ["一", "二", "三", "四"]
-    for year in range(2020, 2022):
-        parameters = {
-            "s": f"{year}年居民可支配收入中位数累计值",  # 搜索整年的数据
-            "m": "searchdata",
-            "db": "",
-            "p": "0"
-        }
-        time.sleep(1)  # 休眠1秒以防止被封
-        try:
-            # 模拟发送请求
-            response = requests.get(url, params=parameters, verify=False)
-            response.raise_for_status()
-            # 获取网页状态码
-            data = response.json()
-
-            # 假设data["result"]是一个包含四个季度数据的列表
-            for i, quarter_data in enumerate(data["result"][:4][::-1]):  # 逆序处理
-                rate_list.append((year, quarters[i], quarter_data["data"]))
-                print(f"{year}年第{quarters[i]}季度居民可支配收入中位数累计值：{quarter_data['data']}")
-
-        except (requests.RequestException, KeyError):
-            print(f"Failed to fetch data for {year}")
-
-
-# 居民人均可支配收入中位数累计增长(季度)
-def spider_Median_per_capita_disposable_income_of_residents_cumulative_growth():
-    url = "https://data.stats.gov.cn/search.htm"
-    rate_list = []  # 使用一个新列表来保存所有数据
-    quarters = ["一", "二", "三", "四"]
-    for year in range(2020, 2022):
-        parameters = {
-            "s": f"{year}年居民可支配收入中位数累计增长",  # 搜索整年的数据
-            "m": "searchdata",
-            "db": "",
-            "p": "0"
-        }
-        time.sleep(1)  # 休眠1秒以防止被封
-        try:
-            # 模拟发送请求
-            response = requests.get(url, params=parameters, verify=False)
-            response.raise_for_status()
-            # 获取网页状态码
-            data = response.json()
-
-            # 假设data["result"]是一个包含四个季度数据的列表
-            for i, quarter_data in enumerate(data["result"][:4][::-1]):  # 逆序处理
-                rate_list.append((year, quarters[i], quarter_data["data"]))
-                print(f"{year}年第{quarters[i]}季度居民可支配收入中位数累计增长：{quarter_data['data']}")
-
-        except (requests.RequestException, KeyError):
-            print(f"Failed to fetch data for {year}")
-
-
-
-# 居民人均可支配收入基尼系数
-def spider_Gini_coefficient_of_per_capita_disposable_income_of_residents():
-    url = "https://data.stats.gov.cn/search.htm"
-    rate_list = []  # 使用一个新列表来保存所有数据
-    for year in (range(2020, 2022)):
-        parameters = {
-            "s": f"{year}居民可支配收入基尼系数",  # f{year}代表格式化字符串
-            "m": "searchdata",
-            "db": "",
-            "p": "0"
-        }
-        time.sleep(1)
-        try:
-            # 模拟发送请求
-            response = requests.get(url, params=parameters, verify=False)
-            response.raise_for_status()
-            # 获取网页状态码
-            data = response.json()
-            rate_list.append((year, data["result"][0]["data"]))
-            print(f"{year}年居民可支配收入基尼系数：{data['result'][0]['data']}")
-        except (requests.RequestException, KeyError):
-            print(f"Failed to fetch data for {year}")
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    spider_disposable_income_of_residents()
-    # spider_Median_per_capita_disposable_income_of_residents()
-    # spider_Gini_coefficient_of_per_capita_disposable_income_of_residents()
-    # spider_disposable_income_of_residents_cumulative_value()
-    # spider_disposable_income_of_residents_cumulative_growth()
-    # spider_Median_per_capita_disposable_income_of_residents_cumulative_value()
-    # spider_Median_per_capita_disposable_income_of_residents_cumulative_growth()
+print("Final data:")
+print(all_data_by_year)
