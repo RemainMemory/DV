1b64b056958cc23549e917b4a512da14d0f34cf3
Remain <chenfeiyu592@gmail.com>
2023-08-27 12:50:18 +0800

    Change the Get_Chart

--- Data_Show/Get_Chart/Chart2.py
+++ Data_Show/Get_Chart/Chart2.py
@@ -4,57 +4,57 @@ from pyecharts.charts import Timeline, Pie
 
 
 # 读取CSV并格式化
-def read_csv_and_format(filename="./Data_csv/disposable_income_national.csv"):
-    df = pd.read_csv(filename)
-    data = {}
-    for index, row in df.iterrows():
-        year_quarter = row['年份_季度']
-        data[year_quarter] = [{"name": col, "value": row[col]} for col in df.columns[1:]]
-    return data
+def generate_Chart2():
+    def read_csv_and_format(filename="./Data_csv/disposable_income_national.csv"):
+        df = pd.read_csv(filename)
+        data = {}
+        for index, row in df.iterrows():
+            year_quarter = row['年份_季度']
+            data[year_quarter] = [{"name": col, "value": row[col]} for col in df.columns[1:]]
+        return data
 
+    # 获取年度_季度饼图
+    def get_year_quarter_pie_chart(year_quarter: str):
+        year_quarter_data = total_data[year_quarter]
+        names = [x["name"] for x in year_quarter_data]
+        values = [x["value"] for x in year_quarter_data]
 
-# 获取年度_季度饼图
-def get_year_quarter_pie_chart(year_quarter: str):
-    year_quarter_data = total_data[year_quarter]
-    names = [x["name"] for x in year_quarter_data]
-    values = [x["value"] for x in year_quarter_data]
-
-    pie = (
-        Pie()
-        .add(
-            series_name="",
-            data_pair=[list(z) for z in zip(names, values)],
-            radius=["20%", "40%"],
-            label_opts=opts.LabelOpts(
-                position="outside",
-                formatter="{b}: {c}"
+        pie = (
+            Pie()
+            .add(
+                series_name="",
+                data_pair=[list(z) for z in zip(names, values)],
+                radius=["20%", "40%"],
+                label_opts=opts.LabelOpts(
+                    position="outside",
+                    formatter="{b}: {c}"
+                )
+            )
+            .set_global_opts(
+                title_opts=opts.TitleOpts(title="Total residents' pension disposable income",
+                                          subtitle="数据来自相关统计局", pos_left="center"),
+                legend_opts=opts.LegendOpts(type_="scroll", pos_left="75%", orient="vertical", pos_top="10%"),
             )
         )
-        .set_global_opts(
-            title_opts=opts.TitleOpts(title="Total residents' pension disposable income", subtitle="数据来自相关统计局", pos_left="center"),
-            legend_opts=opts.LegendOpts(type_="scroll", pos_left="75%", orient="vertical", pos_top="10%"),
-        )
+        return pie
+
+    # 总数据
+    total_data = read_csv_and_format()
+
+    timeline_pie = Timeline(init_opts=opts.InitOpts(width="1000px", height="600px"))
+
+    # 生成饼图
+    for year_quarter in sorted(total_data.keys()):  # 根据你的数据范围调整年份和季度
+        pie = get_year_quarter_pie_chart(year_quarter)
+        timeline_pie.add(pie, time_point=year_quarter)
+
+    timeline_pie.add_schema(
+        is_auto_play=True,
+        play_interval=1000,
+        orient="horizontal",
+        pos_left="10",
+        pos_right="10",
+        pos_bottom="10",
     )
-    return pie
 
-
-# 总数据
-total_data = read_csv_and_format()
-
-timeline_pie = Timeline(init_opts=opts.InitOpts(width="1000px", height="600px"))
-
-# 生成饼图
-for year_quarter in sorted(total_data.keys()):  # 根据你的数据范围调整年份和季度
-    pie = get_year_quarter_pie_chart(year_quarter)
-    timeline_pie.add(pie, time_point=year_quarter)
-
-timeline_pie.add_schema(
-    is_auto_play=True,
-    play_interval=1000,
-    orient="horizontal",
-    pos_left="10",
-    pos_right="10",
-    pos_bottom="10",
-)
-
-timeline_pie.render("disposable_income_by_year_quarter_pie.html")
+    timeline_pie.render("disposable_income_by_year_quarter_pie.html")
--- Data_Show/Get_Chart/Chart3.py
+++ Data_Show/Get_Chart/Chart3.py
@@ -1,88 +1,79 @@
-# 导入图表库，* 匹配所有图表
-from pyecharts.charts import *
-from pyecharts import options as opts
-from pyecharts.faker import Faker
-import csv
-import json
-import pymysql
-# 基本柱状图
 from pyecharts import options as opts
 from pyecharts.charts import Bar, Line
 import csv
 
 
-def read_csv(filename):
-    data = []
-    try:
-        with open(filename, 'r', encoding='utf-8') as csvfile:
-            csvreader = csv.reader(csvfile)
-            next(csvreader)  # Skip the first line (header)
-            for row in csvreader:
-                data.append(row)
-        return data
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-        return []
+def generate_chart3():
+    def read_csv(filename):
+        data = []
+        try:
+            with open(filename, 'r', encoding='utf-8') as csvfile:
+                csvreader = csv.reader(csvfile)
+                next(csvreader)  # Skip the first line (header)
+                for row in csvreader:
+                    data.append(row)
+            return data
+        except FileNotFoundError:
+            print(f"File '{filename}' not found.")
+            return []
 
+    def bar01():
+        # 获取数据
+        data = read_csv("./Data_csv/disposable_income_national.csv")
 
-def bar01():
-    # 获取数据
-    data = read_csv("./Data_csv/disposable_income_national.csv")
+        if len(data) > 0:
+            x_data = [item[0] for item in data]  # Assuming the first column contains the x-axis labels
+            y1_data = [float(item[2]) for item in data]  # Assuming the third column contains data for y1_axis
+            y2_data = [float(item[3]) for item in data]  # Assuming the fourth column contains data for y2_axis
+            y3_data = [float(item[4]) for item in data]  # Assuming the fifth column contains data for y3_axis
+            y4_data = [float(item[5]) for item in data]  # Assuming the sixth column contains data for y4_axis
 
-    if len(data) > 0:
-        x_data = [item[0] for item in data]  # Assuming the first column contains the x-axis labels
-        y1_data = [float(item[2]) for item in data]  # Assuming the third column contains data for y1_axis
-        y2_data = [float(item[3]) for item in data]  # Assuming the fourth column contains data for y2_axis
-        y3_data = [float(item[4]) for item in data]  # Assuming the fifth column contains data for y3_axis
-        y4_data = [float(item[5]) for item in data]  # Assuming the sixth column contains data for y4_axis
+            bar = (
+                Bar()
+                .add_xaxis(xaxis_data=x_data)
+                .add_yaxis(series_name="居民人均可支配工资性收入_累计值", y_axis=y1_data,
+                           label_opts=opts.LabelOpts(is_show=False))
+                .add_yaxis(series_name="居民人均可支配经营净收入_累计值", y_axis=y2_data,
+                           label_opts=opts.LabelOpts(is_show=False))
+                .add_yaxis(series_name="居民人均可支配财产净收入_累计值", y_axis=y3_data,
+                           label_opts=opts.LabelOpts(is_show=False))
+                .add_yaxis(series_name="居民人均可支配转移净收入_累计值", y_axis=y4_data,
+                           label_opts=opts.LabelOpts(is_show=False))
+                .extend_axis(
+                    yaxis=opts.AxisOpts(
+                        name="元",
+                        type_="value",
+                        min_=0,
+                        max_=40000,
+                        interval=5000,
+                        axislabel_opts=opts.LabelOpts(formatter="{value}￥"),
+                    )
+                )
+                .set_global_opts(
+                    title_opts=opts.TitleOpts(title="Disposable income national", subtitle="数据来自相关统计局",
+                                              pos_left="center"),
+                    tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="cross"),
+                    toolbox_opts=opts.ToolboxOpts(is_show=True, orient="vertical", pos_left="left", pos_top="center"),
+                    legend_opts=opts.LegendOpts(pos_bottom="0%")  # 将图例放在图的最下方
+                )
 
-        bar = (
-            Bar()
-            .add_xaxis(xaxis_data=x_data)
-            .add_yaxis(series_name="居民人均可支配工资性收入_累计值", y_axis=y1_data,
-                       label_opts=opts.LabelOpts(is_show=False))
-            .add_yaxis(series_name="居民人均可支配经营净收入_累计值", y_axis=y2_data,
-                       label_opts=opts.LabelOpts(is_show=False))
-            .add_yaxis(series_name="居民人均可支配财产净收入_累计值", y_axis=y3_data,
-                       label_opts=opts.LabelOpts(is_show=False))
-            .add_yaxis(series_name="居民人均可支配转移净收入_累计值", y_axis=y4_data,
-                       label_opts=opts.LabelOpts(is_show=False))
-            .extend_axis(
-                yaxis=opts.AxisOpts(
-                    name="元",
-                    type_="value",
-                    min_=0,
-                    max_=40000,
-                    interval=5000,
-                    axislabel_opts=opts.LabelOpts(formatter="{value}￥"),
+            )
+
+            line = (
+                Line()
+                .add_xaxis(xaxis_data=x_data)
+                .add_yaxis(
+                    series_name="居民人均可支配收入_累计值",
+                    yaxis_index=1,
+                    y_axis=[float(item[1]) for item in data],  # Assuming the second column contains the data
+                    label_opts=opts.LabelOpts(is_show=False),
+
                 )
             )
-            .set_global_opts(
-                title_opts=opts.TitleOpts(title="Disposable income national", subtitle="数据来自相关统计局",
-                                          pos_left="center"),
-                tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="cross"),
-                toolbox_opts=opts.ToolboxOpts(is_show=True, orient="vertical", pos_left="left", pos_top="center"),
-                legend_opts=opts.LegendOpts(pos_bottom="0%")  # 将图例放在图的最下方
-            )
 
-        )
+            bar.overlap(line).render("mixed_bar_and_line.html")
+        else:
+            print("没有查询到数据")
 
-        line = (
-            Line()
-            .add_xaxis(xaxis_data=x_data)
-            .add_yaxis(
-                series_name="居民人均可支配收入_累计值",
-                yaxis_index=1,
-                y_axis=[float(item[1]) for item in data],  # Assuming the second column contains the data
-                label_opts=opts.LabelOpts(is_show=False),
-
-            )
-        )
-
-        bar.overlap(line).render("mixed_bar_and_line.html")
-    else:
-        print("没有查询到数据")
-
-
-if __name__ == "__main__":
-    bar01()
+    if __name__ == "__main__":
+        bar01()
